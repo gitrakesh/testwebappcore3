e5cef5572acfbd80d16d40a4d5eb62a2e018cfa0
Rakesh Yadav <pro.rakeshyadav@outlook.com>
2020-03-28 22:13:19 +0530

    initial commit

---
+++ testwebappcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testwebappcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
